fa13c983725ec5b3073bd6e7febe880c12eab707
vld2405 <liviu.opris@student.unitbv.ro>
2024-11-01 17:02:38 +0200

    Added functionalities to Coordinates Class

--- Eter-MC++/Eter-MC++/Coordinates.cpp
+++ Eter-MC++/Eter-MC++/Coordinates.cpp
@@ -1,5 +1,38 @@
 #include "Coordinates.h"
+#include <functional>
 
 Coordinates::Coordinates() : m_x{ 0 }, m_y{ 0 } {}
 
+Coordinates::Coordinates(int x, int y) : m_x{ x }, m_y{ y } {}
 
+Coordinates::Coordinates(const Coordinates& other) : m_x{ other.m_x }, m_y{ other.m_y } {}
+
+size_t Coordinates::operator()(const Coordinates& key) const
+{
+	return std::hash<int>()(key.m_x) ^ std::hash<int>()(key.m_y);
+}
+
+bool Coordinates::operator==(const Coordinates& other) const
+{
+	return (m_x == other.m_x && m_y == other.m_y);
+}
+
+int Coordinates::GetX() const
+{
+	return m_x;
+}
+
+int Coordinates::GetY() const
+{
+	return m_y;
+}
+
+void Coordinates::SetX(int x)
+{
+	m_x = x;
+}
+
+void Coordinates::SetY(int y)
+{
+	m_y = y;
+}
--- Eter-MC++/Eter-MC++/Coordinates.h
+++ Eter-MC++/Eter-MC++/Coordinates.h
@@ -3,10 +3,16 @@ class Coordinates
 {
 public:
 	Coordinates();
-
+	Coordinates(int x, int y);
+	Coordinates(const Coordinates& other);
+	size_t operator() (const Coordinates& key) const;
+	bool operator== (const Coordinates& other) const;
+	int GetX() const;
+	int GetY() const;
+	void SetX(int x);
+	void SetY(int y);
 
 private:
 	int m_x, m_y;
-
 };
 
