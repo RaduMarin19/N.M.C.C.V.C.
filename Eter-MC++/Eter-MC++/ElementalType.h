7cf470438f507b0cfed3bf782084b44a15038496
RaduMarin19 <165881789+RaduMarin19@users.noreply.github.com>
2024-12-18 09:10:05 +0200

    Update ElementalType.h

--- Eter-MC++/Eter-MC++/ElementalType.h
+++ Eter-MC++/Eter-MC++/ElementalType.h
@@ -11,7 +11,7 @@ enum class ElementalType {
     SQUALL,               // Vijelie: Toate cartile acoperite de alte carti se intorc in mainile proprietarilor.                      !!DONE!!
     HURRICANE,            // Uragan: Shift-eaza un rand complet ocupat cu 1 pozitie in directia dorita.
     FLURRY,               // Rafala: Muta o carte vizibila pe o pozitie adiacenta cu o carte cu un numar mai mic.                      !!DONE!!
-    MIRAGE,               // Miraj: Inlocuieste propria iluzie plasata cu o alta iluzie. Obs: Disponibil doar daca se joaca cu iluzii.
+    MIRAGE,               // Miraj: Inlocuieste propria iluzie plasata cu o alta iluzie. Obs: Disponibil doar daca se joaca cu iluzii. !!DONE!!
     STORM,                // Furtuna: Elimina din joc orice teanc de carti ce contine 2 sau mai multe carti.                           !!DONE!!
     TIDE,                 // Maree: Interschimba pozitiile a 2 teancuri de carti.                                                      !!DONE!!
     MIST,                 // Ceata: Joaca inca o iluzie. Obs: Un jucator nu poate avea 2 iluzii simultan pe tabla.                     !!DONE!! 
