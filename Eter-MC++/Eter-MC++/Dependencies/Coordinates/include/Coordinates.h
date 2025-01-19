94bc8ac11093dead4a863c9999b78667af022cda
RaduMarin19 <marinradul33t@gmail.com>
2025-01-19 15:20:33 +0200

    -Modified project structure -added Coordinates dll and lib to the project

---
+++ Eter-MC++/Eter-MC++/Dependencies/Coordinates/include/Coordinates.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#ifdef EXPORTING_DLL
+#define DLL_API __declspec(dllexport)
+#else
+#define DLL_API __declspec(dllimport)
+#endif
+
+#include <cstddef>
+#include <functional>
+
+class DLL_API Coordinates
+{
+public:
+	struct Hash {
+		size_t operator()(const Coordinates& key) const
+		{
+			return std::hash<int>()(key.m_x) ^ std::hash<int>()(key.m_y);
+		}
+	};
+
+public:
+	Coordinates();
+	Coordinates(int x, int y);
+	Coordinates(const Coordinates& other);
+
+	Coordinates& operator=(const Coordinates& other) = default;
+	Coordinates(Coordinates&& other) noexcept = default;
+	Coordinates& operator=(Coordinates&& other) noexcept = default;
+	~Coordinates() = default;
+
+	bool operator==(const Coordinates& other) const;
+	int GetX() const;
+	int GetY() const;
+	void SetX(int x);
+	void SetY(int y);
+
+private:
+	int m_x, m_y;
+};
